539718a1befae1c314123656201d8368a2698847
LucasBenitez10 <a23lucbensoj@inspedralbes.cat>
2025-03-28 07:11:21 +0100

    refactor

--- cinema-app/next.config.mjs
+++ cinema-app/next.config.mjs
@@ -1,4 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+module.exports = {
+    output: 'export',
+    images: {
+      unoptimized: true
+    }
+  }
